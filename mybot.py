1089126811e90a2b92f37b405b795605662b1115
Aaron B <74215195+AaronB-Hub@users.noreply.github.com>
2022-11-30 19:16:21 +0000

    Python bot

---
+++ mybot.py
@@ -0,0 +1,31 @@
+import random
+
+def new_game():
+  return
+
+def make_move():
+  board = get_board()
+
+  # we found an item! take it!
+  if (board[get_my_x()][get_my_y()] > 0):
+    return TAKE
+
+  rand = random.random() * 4
+
+  if (rand < 1):
+    return NORTH
+  if (rand < 2):
+    return SOUTH
+  if (rand < 3):
+    return EAST
+  if (rand < 4):
+    return WEST
+
+  return PASS
+
+# Optionally include this function if you'd like to always reset to a 
+# certain board number/layout. This is useful for repeatedly testing your
+# bot(s) against known positions.
+#
+#def default_board_number():
+#  return 123
