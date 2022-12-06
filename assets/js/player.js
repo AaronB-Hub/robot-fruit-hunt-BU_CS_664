780b295ec91b3838b5ce94018a87ca7920cc7f49
Aaron B <aaronbo@bu.edu>
2022-12-06 00:25:24 -0500

    Revert "Updated function changes for my bot"
    This reverts commit b08f43f067abd90504a4e9c1b94bd5062f89cab7.

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,26 +1,26 @@
 var GamePlay = {
-    init: function () {
+    init: function() {
         GamePlay.canvas = document.getElementById('game_view');
-        $('.pause').bind('click', function () { GamePlay.mode = "pause"; });
-        $('.play').bind('click', function () { GamePlay.mode = "play"; Board.processMove(); GamePlay.draw(); });
-        $('.forward').bind('click', function () { Board.processMove(); GamePlay.draw(); });
-        $('.newgame').bind('click', function () { GamePlay.setupNewGame(); });
-        $('.reset').bind('click', function () { Board.reset(); });
-        $('#set_board').bind('click', function () { GamePlay.setBoardNumber(); });
-        $('#board_number').bind('keyup', function (e) { if (e.keyCode == 13) { GamePlay.setBoardNumber(); } });
+        $('.pause').bind('click', function() { GamePlay.mode = "pause";});
+        $('.play').bind('click', function() { GamePlay.mode = "play"; Board.processMove(); GamePlay.draw();});
+        $('.forward').bind('click', function() { Board.processMove(); GamePlay.draw();});
+        $('.newgame').bind('click', function() { GamePlay.setupNewGame();});
+        $('.reset').bind('click', function() { Board.reset();});
+        $('#set_board').bind('click', function() { GamePlay.setBoardNumber();});
+        $('#board_number').bind('keyup', function(e) { if(e.keyCode == 13) {GamePlay.setBoardNumber();}});
 
-        $('#check_breadcrumbs').click(function (evt) {
-            if (evt.srcElement.checked) {
-                GamePlay.show_breadcrumbs = true;
-            } else {
-                GamePlay.show_breadcrumbs = false;
-            }
+        $('#check_breadcrumbs').click(function(evt) {
+          if (evt.srcElement.checked) {
+            GamePlay.show_breadcrumbs = true;
+          } else {
+            GamePlay.show_breadcrumbs = false;
+          }
         });
 
         GamePlay.show_breadcrumbs = false;
         var itemImageUrls = ["assets/images/FruitApple.png", "assets/images/FruitBanana.png", "assets/images/FruitCherry.png", "assets/images/FruitMelon.png", "assets/images/FruitOrange.png"];
         GamePlay.itemImages = new Array();
-        for (var i = 0; i < itemImageUrls.length; i++) {
+        for (var i=0; i<itemImageUrls.length; i++) {
             var img = new Image();
             img.src = itemImageUrls[i];
             GamePlay.itemImages[i] = img;
@@ -35,12 +35,12 @@ var GamePlay = {
         GamePlay.bothVisitedImg.src = "assets/images/FruitCellVisitedBoth.png";
         GamePlay.oppVisitedImg = new Image();
         GamePlay.oppVisitedImg.src = "assets/images/FruitCellOppVisited.png";
-        GamePlay.itemImages[itemImageUrls.length - 1].onload = function () {
+        GamePlay.itemImages[itemImageUrls.length - 1].onload = function(){
             GamePlay.setupNewGame();
         };
 
     },
-    setupNewGame: function (boardNumber) {
+    setupNewGame: function(boardNumber) {
         // Create a new board setup according to the following priority:
         // 
         // 1. If a board number is passed in, use that.
@@ -48,8 +48,8 @@ var GamePlay = {
         // 3. Generate a random board number.
         var nextBoardNum;
 
-        if (boardNumber === undefined) {
-            if (typeof default_board_number == 'function' && !isNaN(parseInt(default_board_number()))) {
+        if(boardNumber === undefined) {
+            if ( typeof default_board_number == 'function' && !isNaN(parseInt(default_board_number()))) {
                 nextBoardNum = default_board_number()
             } else {
                 Math.seedrandom();
@@ -64,7 +64,7 @@ var GamePlay = {
         Board.init(nextBoardNum);
 
         Board.newGame();
-        GamePlay.itemTypeCount = get_item_types();
+        GamePlay.itemTypeCount = get_number_of_item_types();
         document.getElementById('grid').width = GamePlay.itemTypeCount * 50 + WIDTH * 50;
         document.getElementById('grid').height = HEIGHT * 50;
         document.getElementById('game_view').width = GamePlay.itemTypeCount * 50 + WIDTH * 50;
@@ -74,95 +74,95 @@ var GamePlay = {
         Grid.draw();
         GamePlay.start();
     },
-    start: function () {
+    start: function() {
         GamePlay.mode = "pause";
         GamePlay.draw();
     },
-    draw: function () {
+    draw: function() {
         var ctx = GamePlay.canvas.getContext('2d');
-        ctx.clearRect(0, 0, GamePlay.canvas.width, GamePlay.canvas.height);
+        ctx.clearRect(0,0,GamePlay.canvas.width,GamePlay.canvas.height);
         GamePlay.drawItems(ctx, Board.board, Board.history);
         GamePlay.drawPlayerTwo(ctx, Board.board);
         GamePlay.drawPlayerOne(ctx, Board.board);
         GamePlay.displayScore(ctx, Board.board);
         if (GamePlay.mode == "play") {
-            var score = Board.checkGameOver();
-            if (score !== undefined) {
-                if (score > 0) {
-                    ctx.font = "30px Arial";
-                    ctx.fillStyle = "#000";
-                    ctx.fillText("You win!", 0, 275);
-                }
-                if (score < 0) {
-                    ctx.font = "30px Arial";
-                    ctx.fillStyle = "#000";
-                    ctx.fillText("You lose!", 0, 275);
-                }
-                if (score == 0) {
-                    ctx.font = "30px Arial";
-                    ctx.fillStyle = "#000";
-                    ctx.fillText("You tie!", 0, 275);
-                }
-                GamePlay.mode = "pause";
-                return;
-            }
-            Board.processMove();
-            setTimeout(function () { GamePlay.draw(); }, 500);
+           var score = Board.checkGameOver();
+           if (score !== undefined) {
+               if (score > 0) {
+                   ctx.font = "30px Arial";
+                   ctx.fillStyle = "#000";
+                   ctx.fillText("You win!", 0, 275);
+               }
+               if (score < 0) {
+                   ctx.font = "30px Arial";
+                   ctx.fillStyle = "#000";
+                   ctx.fillText("You lose!", 0, 275);
+               }
+               if (score == 0) {
+                   ctx.font = "30px Arial";
+                   ctx.fillStyle = "#000";
+                   ctx.fillText("You tie!", 0, 275);
+               }
+               GamePlay.mode = "pause";
+               return;
+           }
+           Board.processMove();
+           setTimeout(function() {GamePlay.draw();}, 500);
         } else {
-            GamePlay.mode = "pause";
+           GamePlay.mode = "pause";
         }
     },
-    displayScore: function (ctx, state) {
+    displayScore: function(ctx, state) {
         ctx.font = "30px Arial";
         ctx.fillStyle = "#366B76";
         ctx.fillText("My Bot", 0, 50);
         ctx.font = "15px Arial";
         ctx.fillStyle = "#000";
-        for (var i = 0; i < GamePlay.itemTypeCount; i++) {
-            ctx.fillText(Board.myBotCollected[i].toFixed(1), 50 * i, 75);
-            ctx.drawImage(GamePlay.itemImages[i], 52 * i + 15, 55, 25, 25);
+        for (var i=0; i<GamePlay.itemTypeCount; i++) {
+            ctx.fillText(Board.myBotCollected[i].toFixed(1), 50*i, 75);
+            ctx.drawImage(GamePlay.itemImages[i], 52*i+15, 55, 25, 25);
         }
         ctx.font = "30px Arial";
         ctx.fillStyle = "#82298E";
         ctx.fillText("Simple Bot", 0, 125);
         ctx.font = "15px Arial";
         ctx.fillStyle = "#000";
-        for (var i = 0; i < GamePlay.itemTypeCount; i++) {
-            ctx.fillText(Board.simpleBotCollected[i].toFixed(1), 50 * i, 150);
-            ctx.drawImage(GamePlay.itemImages[i], 52 * i + 15, 130, 25, 25);
+        for (var i=0; i<GamePlay.itemTypeCount; i++) {
+            ctx.fillText(Board.simpleBotCollected[i].toFixed(1), 50*i, 150);
+            ctx.drawImage(GamePlay.itemImages[i], 52*i+15, 130, 25, 25);
         }
         ctx.font = "30px Arial";
         ctx.fillStyle = "#F00";
         ctx.fillText("items left", 0, 200);
         ctx.font = "15px Arial";
         ctx.fillStyle = "#000";
-        for (var i = 0; i < GamePlay.itemTypeCount; i++) {
-            ctx.fillText((Board.totalItems[i] - Board.myBotCollected[i] - Board.simpleBotCollected[i]).toFixed(1), 50 * i, 225);
-            ctx.drawImage(GamePlay.itemImages[i], 52 * i + 15, 205, 25, 25);
+        for (var i=0; i<GamePlay.itemTypeCount; i++) {
+            ctx.fillText((Board.totalItems[i]-Board.myBotCollected[i]-Board.simpleBotCollected[i]).toFixed(1), 50*i, 225);
+            ctx.drawImage(GamePlay.itemImages[i], 52*i+15, 205, 25, 25);
         }
     },
-    drawPlayerOne: function (ctx, state) {
+    drawPlayerOne: function(ctx, state) {
         ctx.drawImage(GamePlay.player_one_image, GamePlay.itemTypeCount * 50 + Board.myX * 50 + 2, Board.myY * 50 + 2);
     },
-    drawPlayerTwo: function (ctx, state) {
+    drawPlayerTwo: function(ctx, state) {
         ctx.drawImage(GamePlay.player_two_image, GamePlay.itemTypeCount * 50 + Board.oppX * 50 - 2, Board.oppY * 50 - 2);
     },
-    drawItems: function (ctx, state, history) {
-        for (var i = 0; i < WIDTH; i++) {
-            for (var j = 0; j < HEIGHT; j++) {
+    drawItems: function(ctx, state, history) {
+        for (var i=0; i<WIDTH; i++) {
+            for (var j=0; j<HEIGHT; j++) {
                 if (state[i][j] !== 0) {
                     ctx.drawImage(GamePlay.itemImages[state[i][j] - 1], GamePlay.itemTypeCount * 50 + i * 50, j * 50);
-                } else if (GamePlay.show_breadcrumbs && history[i][j] == 1) {
+                } else if (GamePlay.show_breadcrumbs && history[i][j]==1) {
                     ctx.drawImage(GamePlay.visitedImg, GamePlay.itemTypeCount * 50 + i * 50, j * 50);
-                } else if (GamePlay.show_breadcrumbs && history[i][j] == 2) {
+                } else if (GamePlay.show_breadcrumbs && history[i][j]==2) {
                     ctx.drawImage(GamePlay.oppVisitedImg, GamePlay.itemTypeCount * 50 + i * 50, j * 50);
-                } else if (GamePlay.show_breadcrumbs && history[i][j] == 3) {
+                } else if (GamePlay.show_breadcrumbs && history[i][j]==3) {
                     ctx.drawImage(GamePlay.bothVisitedImg, GamePlay.itemTypeCount * 50 + i * 50, j * 50);
                 }
             }
         }
     },
-    setBoardNumber: function () {
+    setBoardNumber: function() {
         var boardNumber;
 
         boardNumber = parseInt($('#board_number').val());
